42182576916cbed262390a55f26a0811fad38d2f
kimutai Elphas <33074564+kimutaiRop@users.noreply.github.com>
2019-01-24 18:18:53 +0300

    Update config.php

--- theme/config.php
+++ theme/config.php
@@ -2,7 +2,7 @@
 <?php
    define('DB_SERVER', 'localhost'); //if production change jostname
    define('DB_USERNAME', 'root'); // root or any other super user database username that exist
-   define('DB_PASSWORD', '@kymR0p.');  //place database password here
+   define('DB_PASSWORD', 'toor');  //place database password here
    define('DB_DATABASE', 'marie');  //place database name 
    $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 ?>
