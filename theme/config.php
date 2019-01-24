b3f71b27db9a24376b1c529c12152a65eee78d48
kimutai Elphas <33074564+kimutaiRop@users.noreply.github.com>
2019-01-24 18:19:49 +0300

    Update config.php

--- theme/config.php
+++ theme/config.php
@@ -1,6 +1,6 @@
 <?php ini_set('display_errors', 1); ?>
 <?php
-   define('DB_SERVER', 'localhost'); //if production change jostname
+   define('DB_SERVER', 'localhost'); //if production change hostname
    define('DB_USERNAME', 'root'); // root or any other super user database username that exist
    define('DB_PASSWORD', 'toor');  //place database password here
    define('DB_DATABASE', 'marie');  //place database name 
