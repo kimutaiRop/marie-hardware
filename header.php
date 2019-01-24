f6a7965e8d6acc1c4925719f644ae95e7fabff6c
Elphas <kimrop19@gmail.com>
2019-01-24 21:12:36 +0300

    make it better

--- header.php
+++ header.php
@@ -1,81 +1,21 @@
 <?php
-include('config.php');
+    // make table called users with fields idusers, username, first_name, last_name.
+   include("header.php");
+   $error = "";
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+      //get username and password sent from form 
+      
+      $first_name = mysqli_real_escape_string($db,$_POST['first_name']);
+      $last_name = mysqli_real_escape_string($db,$_POST['last_name']);
+      $myusername = mysqli_real_escape_string($db,$_POST['username']);
+      $mypassword_1 = mysqli_real_escape_string($db,$_POST['password1']); 
+      $mypassword_2= mysqli_real_escape_string($db,$_POST['password2']);
+      if($mypassword_1 == $mypassword_1){
+        $mypassword = $mypassword_1;
+         $sql = "INSERT INTO `users` (`username`, `password`, `first_name`, `last_name`) VALUES ('$myusername', '$mypassword', '$first_name', '$last_name')";
+         mysqli_query($db,$sql);
+         header("location: index.php");
+      }
+   }
 ?>
-<div class="pre-header">
-        <div class="container">
-            <div class="row">
-                <!-- BEGIN TOP BAR LEFT PART -->
-                <div class="col-md-6 col-sm-6 additional-shop-info">
-                    <ul class="list-unstyled list-inline">
-                        <li><i class="fa fa-phone"></i><span>+254 707 079 023</span></li>
-                        <li><i class="fa fa-envelope-o"></i><span>info@mariehardware.com</span></li>
-                    </ul>
-                </div>
-                <!-- END TOP BAR LEFT PART -->
-                <!-- BEGIN TOP BAR MENU -->
-                <div class="col-md-6 col-sm-6 additional-nav">
-                    <ul class="list-unstyled list-inline pull-right">
-                        <li><a href="page-login.php">Log In</a></li>
-                        <li><a href="page-reg-page.php">Registration</a></li>
-                    </ul>
-                </div>
-                <!-- END TOP BAR MENU -->
-            </div>
-        </div>        
-    </div>
-<div class="header">
-      <div class="container">
-        <a class="site-logo" href="index.php"><img src="assets/corporate/img/logos/logo-corp-red.png" alt="Metronic FrontEnd"></a>
-
-        <a href="javascript:void(0);" class="mobi-toggler"><i class="fa fa-bars"></i></a>
-
-        <!-- BEGIN NAVIGATION -->
-        <div class="header-navigation pull-right font-transform-inherit">
-          <ul>
-            <li class="">
-              <a class="dropdown-toggle"   href="index.php">
-                Home 
-                
-              </a>
-              
-            </li>
-           
-
-
-            <li class="dropdown">
-              <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="javascript:;">
-                Pages 
-                
-              </a>
-                
-              <ul class="dropdown-menu">
-                <li><a href="page-about.php">About Us</a></li>
-                <li><a href="page-login.php">Login Page</a></li>
-                <li><a href="page-forgotton-password.php">Forget Password</a></li>
-                <li><a href="page-reg-page.php">Signup Page</a></li>
-                <li><a href="page-contacts.php">Contact</a></li>                
-              </ul>
-            </li>
-          
-
-            <!-- BEGIN TOP SEARCH -->
-            <li class="menu-search">
-              <span class="sep"></span>
-              <i class="fa fa-search search-btn"></i>
-              <div class="search-box">
-                <form action="#">
-                  <div class="input-group">
-                    <input type="text" placeholder="Search" class="form-control">
-                    <span class="input-group-btn">
-                      <button class="btn btn-primary" type="submit">Search</button>
-                    </span>
-                  </div>
-                </form>
-              </div> 
-            </li>
-            <!-- END TOP SEARCH -->
-          </ul>
-        </div>
-        <!-- END NAVIGATION -->
-      </div>
-    </div>
